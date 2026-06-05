bfe55a4450bf7bfcaa1e416156eea7895d1d7c4e
Kim Chit <chitkim871@gmail.com>
2026-06-05 18:02:52 +0700

    fix color celsuis

--- weather-app/src/components/ForecastCard.jsx
+++ weather-app/src/components/ForecastCard.jsx
@@ -1,32 +1,42 @@
 function ForecastCard({ forecast }) {
+  const getTempColor = (temp) => {
+    if (temp <= 10) return "from-blue-700 via-blue-500";
+    if (temp <= 20) return "from-sky-500 via-cyan-400";
+    if (temp <= 30) return "from-yellow-400 via-orange-400";
+    return "from-yellow-500 via-red-500";
+  };
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
-      {forecast.map((day, index) => (
-        <div
-          key={index}
-          className="bg-white/20 backdrop-blur-lg border border-white/20 rounded-2xl p-4 text-center text-white"
-        >
-          <h3 className="font-semibold mb-2">
-            {new Date(day.dt_txt).toLocaleDateString("en-US", {
-              weekday: "short",
-            })}
-          </h3>
+      {forecast.map((day, index) => {
+        const temp = Math.round(day.main.temp);
 
-          <img
-            src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-            alt=""
-            className="mx-auto w-16 h-16"
-          />
+        return (
+          <div
+            key={index}
+            className={`bg-white/20 backdrop-blur-lg border border-white/30 bg-linear-to-r ${getTempColor(
+              temp
+            )} rounded-2xl p-4 text-center text-white`}
+          >
+            <h3 className="font-semibold mb-2">
+              {new Date(day.dt_txt).toLocaleDateString("en-US", {
+                weekday: "short",
+              })}
+            </h3>
 
-          <h2 className="text-xl md:text-2xl font-bold">
-            {Math.round(day.main.temp)}°C
-          </h2>
+            <img
+              src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+              alt=""
+              className="mx-auto w-16 h-16"
+            />
 
-          <p className="text-xs md:text-sm capitalize">
-            {day.weather[0].description}
-          </p>
-        </div>
-      ))}
+            <h2 className="text-xl md:text-2xl font-bold">{temp}°C</h2>
+
+            <p className="text-xs md:text-sm capitalize">
+              {day.weather[0].description}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- weather-app/src/components/HourlyForecast.jsx
+++ weather-app/src/components/HourlyForecast.jsx
@@ -1,41 +1,53 @@
 function HourlyForecast({ forecast }) {
+    const getTempColor = (temp) => {
+        if (temp <= 10) return "from-blue-700 via-blue-500";
+        if (temp <= 20) return "from-sky-500 via-cyan-400";
+        if (temp <= 30) return "from-yellow-400 via-orange-400";
+        return "from-yellow-500 via-red-500";
+    };
+
     return (
         <div className="bg-white/10 backdrop-blur-xl rounded-3xl p-5">
-            {/* <h2 className="text-white text-2xl font-bold mb-4">
-                Hourly Forecast
-            </h2> */}
-
             <div className="flex gap-4 overflow-x-auto pb-2">
-                {forecast.map((item, index) => (
-                    <div
-                        key={index}
-                        className="min-w-35 bg-white/10 rounded-2xl p-4 text-center shrink-0"
-                    >
-                        <p className="text-white font-semibold">
-                            {index === 0
-                                ? "Now"
-                                : new Date(item.dt * 1000).toLocaleTimeString("en-US", {
-                                    hour: "numeric",
-                                    minute: "2-digit",
-                                    hour12: true,
-                                })}
-                        </p>
+                {forecast.map((item, index) => {
+                    const temp = Math.round(item.main.temp);
 
-                        <img
-                            src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-                            alt={item.weather[0].description}
-                            className="mx-auto"
-                        />
+                    return (
+                        <div
+                            key={index}
+                            className={`min-w-35 shrink-0 rounded-2xl p-4 text-center bg-linear-to-r ${getTempColor(
+                                temp
+                            )} text-white`}
+                        >
+                            <p className="font-semibold">
+                                {index === 0
+                                    ? "Now"
+                                    : new Date(item.dt * 1000).toLocaleTimeString(
+                                          "en-US",
+                                          {
+                                              hour: "numeric",
+                                              minute: "2-digit",
+                                              hour12: true,
+                                          }
+                                      )}
+                            </p>
 
-                        <h3 className="text-white text-xl font-bold">
-                            {Math.round(item.main.temp)}°C
-                        </h3>
+                            <img
+                                src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                                alt={item.weather[0].description}
+                                className="mx-auto"
+                            />
 
-                        <p className="text-white/80 text-sm">
-                            {item.weather[0].main}
-                        </p>
-                    </div>
-                ))}
+                            <h3 className="text-xl font-bold">
+                                {temp}°C
+                            </h3>
+
+                            <p className="text-sm text-white/90">
+                                {item.weather[0].main}
+                            </p>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
